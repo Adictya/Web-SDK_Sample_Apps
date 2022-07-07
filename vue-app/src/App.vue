1884d1aca03d4b2c22f71afd49061bfd682fa27d
adictya <aditya.26101@gmail.com>
2022-07-07 13:21:49 +0530

    added vue app with jsx support

---
+++ vue-app/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div style="height: 100vh; width: 100vw">
+    <app-builder />
+  </div>
+</template>
+
+<script>
+import AgoraAppBuilder from "agora-app-builder-sdk";
+// eslint-disable-next-line
+import React from "react";
+// To prevent react getting removed in dead code elimination step;
+React.createElement("div");
+
+const Create = () => {
+  return <div>Create screen</div>;
+};
+
+const FPE = AgoraAppBuilder.createFPE({
+  components: {
+    create: PreCall,
+  },
+});
+
+AgoraAppBuilder.addFPE(FPE);
+
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
