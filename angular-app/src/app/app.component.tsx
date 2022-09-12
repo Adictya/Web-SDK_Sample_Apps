8b099483811a1485433cda8c1deca434e37bce69
Aditya Rathore <aditya.26101@gmail.com>
2022-09-12 15:57:36 +0530

    added event subs to angular and react sample apps

--- angular-app/src/app/app.component.tsx
+++ angular-app/src/app/app.component.tsx
@@ -1,68 +1,74 @@
-import React from 'react';
-import { Component } from '@angular/core';
-import fpe from './test-fpe'
-import AgoraAppBuilder from 'agora-app-builder-sdk';
+import React from "react";
+import { Component } from "@angular/core";
+import AgoraAppBuilder from "agora-app-builder-sdk";
+import fpe from "./test-fpe";
 
 // Prevent dead code elimination on react
-React.createElement('div');
+React.createElement("div");
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
-    <div >
+    <div>
       <div style="height: 1.5rem">
         Join a meeting:
-        <input id="meetingId" placeholder="meetingId"/>
-        <button
-          (click)="JoinMeeting()"
-        >
-          Join
-        </button>
+        <input id="meetingId" placeholder="meetingId" />
+        <button (click)="JoinMeeting()">Join</button>
       </div>
       <div style="height: calc(100vh - 1.5rem)">
-        <app-builder></ app-builder>
+        <app-builder></app-builder>
       </div>
     </div>
   `,
-  styleUrls: ['./app.component.css'],
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'angular-app';
-
+  title = "angular-app";
+  unsubs: Array<any> = [];
   ngOnInit() {
     AgoraAppBuilder.addFPE(fpe);
 
-    AgoraAppBuilder.on(
-      'create',
-      (hostMeetingId, attendeeMeetingId, pstnNumber) => {
-        console.log('Meeting created with', {
-          hostMeetingId,
-          attendeeMeetingId,
-          pstnNumber,
+    this.unsubs = [
+      AgoraAppBuilder.on(
+        "create",
+        (hostMeetingId, attendeeMeetingId, pstnNumber) => {
+          console.log("Angular Host App: Meeting created with", {
+            hostMeetingId,
+            attendeeMeetingId,
+            pstnNumber,
+          });
+        }
+      ),
+      AgoraAppBuilder.on("create", () => {
+        console.log("Angular Host App: Meeting created queued event");
+      }),
+      AgoraAppBuilder.on("preJoin", (meetingTitle, deviceList) => {
+        console.log("Angular Host App: precall with", {
+          meetingTitle,
+          deviceList,
         });
-      }
-    );
-    AgoraAppBuilder.on('preJoin', (meetingTitle, deviceList) => {
-      console.log('in meeting precall with', { meetingTitle, deviceList });
-    });
-    AgoraAppBuilder.on('join', (meetingTitle, deviceList, isHost) => {
-      console.log('Meeting joined with', { meetingTitle, deviceList, isHost });
-    });
-    AgoraAppBuilder.on('leave', () => {
-      console.log('Meeting left');
-    });
+      }),
+      AgoraAppBuilder.on("join", (meetingTitle, deviceList, isHost) => {
+        console.log("Angular Host App: joined with", {
+          meetingTitle,
+          deviceList,
+          isHost,
+        });
+      }),
+      AgoraAppBuilder.on("leave", () => {
+        console.log("Angular Host App: left");
+      }),
+    ];
   }
 
   JoinMeeting() {
     AgoraAppBuilder.joinMeeting(
-      // document.getElementById('meetingId')!.nodeValue!
-      '5b512f7d-d717-4dc5-84f2-eaa43133c798'
+      document.getElementById("meetingId")!.nodeValue!
     );
   }
-  ngOnDestroy(){
-    AgoraAppBuilder.off("create");
-    AgoraAppBuilder.off("preJoin");
-    AgoraAppBuilder.off("join");
-    AgoraAppBuilder.off("leave");
+  ngOnDestroy() {
+    this.unsubs.forEach((v) => {
+      v();
+    });
   }
 }
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,20 +1,69 @@
-import React, { useEffect, useState } from "react";
-// import fpe from "./test-fpe";
+import React, { useEffect } from "react";
+import fpe from "./test-fpe";
 import AppBuilderMethods from "agora-app-builder-sdk";
 
 function App() {
   useEffect(() => {
-    AppBuilderMethods.on("join", (tet, _, isHost) => {
-      console.log("I got join", tet, isHost);
-    });
-    AppBuilderMethods.on("preJoin", (title) => {
-      console.log("I got preJoin", title);
-    });
-    AppBuilderMethods.addFPE({});
+    const unsubs: Array<any> = [
+      AppBuilderMethods.on(
+        "create",
+        (hostMeetingId, attendeeMeetingId, pstnNumber) => {
+          console.log("React Host App: Meeting created with", {
+            hostMeetingId,
+            attendeeMeetingId,
+            pstnNumber,
+          });
+        }
+      ),
+      AppBuilderMethods.on("create", () => {
+        console.log("React Host App: Meeting created queued event");
+      }),
+      AppBuilderMethods.on("preJoin", (meetingTitle, deviceList) => {
+        console.log("React Host App: precall with", {
+          meetingTitle,
+          deviceList,
+        });
+      }),
+      AppBuilderMethods.on("join", (meetingTitle, deviceList, isHost) => {
+        console.log("React Host App: joined with", {
+          meetingTitle,
+          deviceList,
+          isHost,
+        });
+      }),
+      AppBuilderMethods.on("leave", () => {
+        console.log("React Host App: left");
+      }),
+    ];
+    return () => {
+      unsubs.forEach((v) => {
+        if (typeof v === "function") v();
+      });
+    };
   }, []);
+
+  const joinMeeting = () => {
+    AppBuilderMethods.joinMeeting(
+      document.getElementById("meetingId")!.nodeValue!
+    );
+  };
+
   return (
-    <div style={{ display: "flex", height: "100vh", width: "100vw" }}>
-      <AppBuilderMethods.View />
+    <div>
+      <div style={{ height: "1.5rem" }}>
+        Join a meeting:
+        <input id="meetingId" placeholder="meetingId" />
+        <button onClick={joinMeeting}>Join</button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          height: "calc( 100vh - 1.5rem )",
+          width: "100vw",
+        }}
+      >
+        <AppBuilderMethods.View />
+      </div>
     </div>
   );
 }
